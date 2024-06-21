522b27f29ed4e01755cd5a215db3171c8a54fd3e
JDG <160174648+Nihilusone@users.noreply.github.com>
2024-06-20 18:58:20 -0700

    update foxholebot.py -added fancy ASCII Logo -added license text -improved bot token logic

--- foxholebot.py
+++ foxholebot.py
@@ -1,18 +1,29 @@
-import os
 import os.path
 import json
-import discord
 
-botTokenPath='./json/botToken.json'
+# Foxhole Discord Bot By Tuxmasku
+# Licensed under GNU
+# Ver 0.0.1
 
+# Constants
+botTokenPath = './json/botToken.json'
 
-print("FoxHole Discord bot by Tuxmasku ver 0.0.1")
+print("""
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░██╗░░░░░██████╗░██████╗░░█████╗░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░░░██║██║░░░░░██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║██║░░░░░██║░░██║██████╔╝███████║██║░░╚═╝██║░░░██║██║░░░░░███████║
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║██║░░░░░██║░░██║██╔══██╗██╔══██║██║░░██╗██║░░░██║██║░░░░░██╔══██║
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║███████╗██████╔╝██║░░██║██║░░██║╚█████╔╝╚██████╔╝███████╗██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝""")
+print("Foxhole Discord bot by Tuxmasku")
+print("Ver 0.0.1 Licensed under GPL-3.0")
 
+# Determine if token file is present and prompts user to create it if it does not exist.
 if os.path.isfile(botTokenPath) and os.access(botTokenPath, os.R_OK):
     print("Bot Token File is present proceeding...")
 else:
-    botToken=input("Please input your bot token: ")
-    tokenData={"token": botToken}
-    jsonData=json.dumps(tokenData)
+    botToken = input("Please input your bot token: ")   # requests input from user to place discord bot token
+    tokenData = {"token": botToken}
+    jsonData = json.dumps(tokenData)
     with open(botTokenPath, "w") as jsonFile:
         json.dump(tokenData, jsonFile)
